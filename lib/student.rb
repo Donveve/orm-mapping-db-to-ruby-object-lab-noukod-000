2fd96fcb59564b954c5c679870b644536f6e04b1
Veniel Dorestave <veniel@noukod.com>
2018-11-16 17:23:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,8 +48,8 @@ def self.all_students_in_grade_X(x)
 end
 # def save
 #   sql = <<-SQL
-#     INSERT INTO students (name, grade) 
-# 
+#     INSERT INTO students (name, grade)
+#
 # #   def self.new_from_db(row)
 # #     # create a new Student object given a row from the database
 # #     new_student = self.new
@@ -58,7 +58,7 @@ end
 # #     new_student.grade = row[2]
 # #     new_student
 # #   end
-# # 
+# #
 # #   def self.all
 # #     # retrieve all the rows from the "Students" database
 # #     # remember each row should be a new instance of the Student class
@@ -67,7 +67,7 @@ end
 # #       self.new_from_db(row)
 # #     end
 # #   end
-# # 
+# #
 # #   def self.find_by_name(name)
 # #     # find the student in the database given a name
 # #     # return a new instance of the Student class
@@ -75,7 +75,7 @@ end
 # #     student_row = DB[:conn].execute(sql, name)[0]
 # #     self.new_from_db(student_row)
 # #   end
-# # 
+# #
 # #   # def self.count_all_students_in_grade_9
 # #   #    sql = <<-SQL
 # #   #    SELECT * FROM students WHERE grade = "9"
@@ -145,16 +145,16 @@ end
 # #     self.new_from_db(row)
 # #   end.first
 # # end
-# # 
+# #
 # #   def save
 # #     sql = <<-SQL
 # #       INSERT INTO students (name, grade)
 # #       VALUES (?, ?)
 # #     SQL
-# # 
+# #
 # #     DB[:conn].execute(sql, self.name, self.grade)
 # #   end
-# # 
+# #
 # #   def self.create_table
 # #     sql = <<-SQL
 # #     CREATE TABLE IF NOT EXISTS students (
@@ -163,22 +163,22 @@ end
 # #       grade TEXT
 # #     )
 # #     SQL
-# # 
+# #
 # #     DB[:conn].execute(sql)
 # #   end
-# # 
+# #
 # #   def self.drop_table
 # #     sql = "DROP TABLE IF EXISTS students"
 # #     DB[:conn].execute(sql)
 # #   end
 # # end
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
+#
+#
 # # require 'pry'
 # # class Student
 # #   attr_accessor :id, :name, :grade
