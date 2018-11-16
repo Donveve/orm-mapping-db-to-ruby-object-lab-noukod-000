ab3e56d96276ffd26821e1e2ff5502233f9c17cc
Veniel Dorestave <veniel@noukod.com>
2018-11-16 17:07:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,17 +28,21 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
-  sql = <<-SQL
-  SELECT * FROM students WHERE grade = "9"
-  SQL
-  DB[:conn].execute(sql)
-end
+     sql = <<-SQL
+     SELECT * FROM students WHERE grade = "9"
+     SQL
+     DB[:conn].execute(sql)
+   end
+  # def self.count_all_students_in_grade_9
+  #   sql = "SELECT * FROM students WHERE grade = 9"
+  #   DB[:conn].execute(sql)
+  # end
 
   def self.students_below_12th_grade
     sql = "SELECT * FROM students WHERE grade < 12"
     DB[:conn].execute(sql)
   end
-
+  
   def self.first_x_students_in_grade_10(x)
     sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
     DB[:conn].execute(sql, x)
@@ -57,13 +61,13 @@ end
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade)
+      INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-
+  
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -91,7 +95,7 @@ end
 # require 'pry'
 # class Student
 #   attr_accessor :id, :name, :grade
-#
+# 
 #   def self.new_from_db(row)
 #     student = self.new
 #     student.id = row[0]
@@ -99,7 +103,7 @@ end
 #     student.grade = row[2]
 #     student
 #   end
-#
+# 
 #   def self.all
 #     sql = <<-SQL
 #     SELECT *
@@ -109,7 +113,7 @@ end
 #       self.new_from_db(row)
 #     end
 #   end
-#
+# 
 #   def self.find_by_name(name)
 #     sql = <<-SQL
 #     SELECT *
@@ -121,17 +125,17 @@ end
 #       self.new_from_db(row)
 #     end.first
 #   end
-#
-#
+# 
+# 
 #   def save
 #     sql = <<-SQL
 #       INSERT INTO students (name, grade)
 #       VALUES (?, ?)
 #     SQL
-#
+# 
 #     DB[:conn].execute(sql, self.name, self.grade)
 #   end
-#
+# 
 #   def self.create_table
 #     sql = <<-SQL
 #     CREATE TABLE IF NOT EXISTS students (
@@ -140,10 +144,10 @@ end
 #       grade TEXT
 #     )
 #     SQL
-#
+# 
 #     DB[:conn].execute(sql)
 #   end
-#
+# 
 #   def self.drop_table
 #     sql = "DROP TABLE IF EXISTS students"
 #     DB[:conn].execute(sql)
